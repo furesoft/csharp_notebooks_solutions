2fdfd0992565a406294c1f27d9db86e30079e363
nico.xyz1 <nic.garciab@duocuc.cl>
2025-01-11 05:22:31 -0300

    Solutions for Read/Write files exercises

---
+++ Solutions-crow/ReadWriteFiles/Highscores/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Text;
+
+class Program
+{
+    static string path = @"D:\test\leaderboard.txt";
+    static void Main()
+    {
+        if (!File.Exists(path)){
+            Console.WriteLine("Enter your name: ");
+            var name = Convert.ToString(Console.ReadLine());
+            Console.WriteLine("Enter your score: ");
+            var score = Convert.ToInt32(Console.ReadLine());
+            var line = $"{name} - {score}";
+            File.WriteAllText(path, line);
+        }
+        else
+        {
+            GetScores();
+            Console.WriteLine("Enter your name: ");
+            var name = Convert.ToString(Console.ReadLine());
+            Console.WriteLine("Enter your score: ");
+            var score = Convert.ToInt32(Console.ReadLine());
+            var line = $"\n{name} - {score}";
+            File.AppendAllText(path, line);
+        }
+    }
+    static void GetScores()
+    {
+        string[] lines = File.ReadAllLines(path);
+        List<(string fullLine, int score)> scoreList = new List<(string fullLine, int score)>();
+        int place = 1;
+
+        foreach (string line in lines)
+        {
+            string[] parts = line.Split("-"); //Split the line at the "-" to extract the score
+            if (parts.Length >= 2 && int.TryParse(parts[1].Trim(), out int score)) //Ensure correct formatting
+            {
+                scoreList.Add((line, score)); //Add the full line and score as a tuple to the list
+            }
+        }
+        scoreList = scoreList.OrderByDescending(item => item.score).ToList();
+        Console.WriteLine("Leaderboard:");
+        
+        foreach (var entry in scoreList) //Display the full score line "place. name - score"
+        {
+            Console.WriteLine($"{place}. {entry.fullLine}");
+            place++;
+        }
+    }
+}
---
+++ Solutions-crow/ReadWriteFiles/Logger/Program.cs
@@ -0,0 +1,23 @@
+﻿class Program
+{
+    public static void Main()
+    {
+        string path = @"D:\test\logger.txt";
+        if (!File.Exists(path)) //If file doesn't exist
+        {
+            Console.WriteLine("Enter a message...");
+            var input = Convert.ToString(Console.ReadLine());
+            var date = System.DateTime.Now;
+            string message = $"[{date}] {input}.";
+            File.WriteAllText(path, message);
+        }
+        else
+        {
+            Console.WriteLine("Enter a message...");
+            var input = Convert.ToString(Console.ReadLine());
+            var date = System.DateTime.Now;
+            string message = $"\n[{date}] {input}.";
+            File.AppendAllText(path, message);
+        }
+    }
+}
